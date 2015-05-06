38a2e3ffc723a239b30643fcc3f0608648969b25
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2015-05-06 02:44:23 +0000

    Unzip directly

--- nppes_fhir_demo/load_data.sh
+++ nppes_fhir_demo/load_data.sh
@@ -2,11 +2,10 @@
 
 if [ ! -e "/data/npidata_20050523-20150412.csv" ]
 then
-    cd /tmp
+    cd /data
     wget http://nppes.viva-it.com/NPPES_Data_Dissemination_April_2015.zip
     unzip NPPES_Data_Dissemination_April_2015.zip
     rm NPPES_Data_Dissemination_April_2015.zip
-    mv npidata_20050523-20150412.csv /data
 fi
 
 if [ ! -e "/data/nucc_taxonomy_150.csv" ]
